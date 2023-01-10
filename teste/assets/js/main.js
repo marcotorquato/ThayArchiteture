0706372d376b0fc61bc2615670cc6719de28f543
Marco Antonio Torquato <marcotorquato97@gmail.com>
2023-01-10 17:04:23 +0100

    typed js

--- teste/assets/js/main.js
+++ teste/assets/js/main.js
@@ -67,6 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
   }
 
+  
   /**
    * Initiate glightbox
    */
@@ -187,3 +188,21 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+
+/**
+   * Hero type effect
+   */
+const typed = select('.typed')
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items')
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
+
