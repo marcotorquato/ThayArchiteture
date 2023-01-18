4a2131778e83254041e34b0ace60ed441a7b5f6d
Marco Antonio Torquato <marcotorquato97@gmail.com>
2023-01-18 23:37:02 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,9 +75,75 @@ document.addEventListener('DOMContentLoaded', () => {
     selector: '.glightbox'
   });
 
+
   /**
    * Porfolio isotope and filter
    */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+    /**
+   * Initiate portfolio lightbox 
+   */
+    const portfolioLightbox = GLightbox({
+      selector: '.portfolio-lightbox'
+    });
+  
+    /**
+     * Initiate portfolio details lightbox 
+     */
+    const portfolioDetailsLightbox = GLightbox({
+      selector: '.portfolio-details-lightbox',
+      width: '90%',
+      height: '90vh'
+    });
+  
+    /**
+     * Portfolio details slider
+     */
+    new Swiper('.portfolio-details-slider', {
+      speed: 400,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    });
+
+  
+
+  /**
+   * Porfolio isotope and filter
+   
   let portfolionIsotope = document.querySelector('.portfolio-isotope');
 
   if (portfolionIsotope) {
@@ -108,9 +174,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }, false);
       });
 
-    });
+    });  
 
-  }
+  }*/
 
   /**
    * Init swiper slider with 1 slide at once in desktop view
