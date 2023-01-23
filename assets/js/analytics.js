1b2de8fe4c6a2d8b595368c8e55987b9e846f51b
Marco Antonio Torquato <marcotorquato97@gmail.com>
2023-01-23 19:06:11 +0100

    add analytics

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,6 +1,6 @@
 
-window.dataLayer = window.dataLayer || [];
+  window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-KX6MHKYSLF');
+  gtag('config', 'G-PLQ7VQ881W');
